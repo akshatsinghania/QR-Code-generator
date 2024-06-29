4df97d8bd740331c65bf459c33fb54af8ad882e0
akshatsinghania <singhaniaakshat1@gmail.com>
2024-06-29 22:07:02 +0530

    init project

---
+++ qr-code-generator/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>QR Code Generator</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
